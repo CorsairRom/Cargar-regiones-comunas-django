029c642811ba8e9b51ea55c695b03ee8307b3c54
CorsairRom <ric.romero@duocuc.cl>
2023-07-21 03:40:53 -0400

    add: countries

---
+++ extraerPaises.py
@@ -0,0 +1,24 @@
+import json
+from os import system
+
+system('cls')
+
+with open('countries.json', 'r', encoding='utf-8') as file:
+    countries = json.load(file)
+    
+    
+countryDjango = []
+
+for country in countries['countries']:
+    data = {}
+    fields = {}
+    data['model'] = 'acisApi.countries'
+    nombre = country['name_es']
+    codigo = country['code_2']
+    fields['name'] = nombre
+    fields['code'] = codigo
+    data['fields'] = fields
+    countryDjango.append(data)
+
+with open('countries-Django.json', 'w', encoding='utf-8') as outfile:
+    json.dump(countryDjango, outfile, ensure_ascii=False, indent=4)
